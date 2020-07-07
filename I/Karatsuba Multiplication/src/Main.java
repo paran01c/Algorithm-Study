8bf5b0cb12c46d0daa27a9785a71d8986df80e46
paran01c <37111262+paran01c@users.noreply.github.com>
2020-07-07 20:31:11 +0300

    Creating the array branch

--- I/Karatsuba Multiplication/src/Main.java
+++ I/Karatsuba Multiplication/src/Main.java
@@ -4,64 +4,34 @@ public class Main {
 
     public static long karatsubaMultiply(long x, long y) {
 
-        // digitsX need to be equal to digits y
-        //gets the digits of the 2 numbers
         long digitsX = numberDigits(x);
         long digitsY = numberDigits(y);
+        //m = power of 10 used for the algorithm
+        long m = 0;
+        long minDigits = 0;
 
-        // base case
-        if(digitsX == 1|| digitsY == 1) {
-
-            return x * y;
-
-        } else {
-
-            //decomposes the numbers as (10^n * a + b)
-            long digitsComponents = digitsX / 2;
-            long a = x / (long)Math.pow(10, digitsComponents);
-            digitsComponents = digitsX - digitsComponents;
-            long b = 0;
-            if(digitsX % 2 == 0) {
-                b = x % (long) Math.pow(10, digitsComponents);
-            } else {
-                b = x % (long) Math.pow(10, digitsComponents - 1);
-            }
-
-            digitsComponents = digitsY / 2;
-            long c = y / (long)Math.pow(10, digitsComponents);
-            digitsComponents = digitsY - digitsComponents;
-
-            long d = 0;
-            if(digitsY % 2 == 0) {
-                d = y % (long) Math.pow(10, digitsComponents);
-            } else {
-                d = y % (long) Math.pow(10, digitsComponents - 1);
-            }
-
-            System.out.println("-------");
-            System.out.println(a);
-            System.out.println(b);
-            System.out.println(c);
-            System.out.println(d);
-            System.out.println("-------");
-
-            long ac = karatsubaMultiply(a, c);
-            System.out.println("!!!!!!!!!!!!!!");
-            System.out.println("ac" + ac);
-            long bd = karatsubaMultiply(b, d);
-            System.out.println("!!!!!!!!!!!!!!");
-            System.out.println("bd" + bd);
-            long all = karatsubaMultiply((a + b), (c + d));
-            System.out.println("!!!!!!!!!!!!!!");
-            System.out.println("all" + all);
-
-            System.out.println("?????????");
-            long k = (long)(Math.pow(10, numberDigits(bd)) * ac) + (long)(Math.pow(10, (int)numberDigits(bd) / 2) * (all - ac - bd)) + bd;
-            System.out.println(k);
-            System.out.println("?????????");
-            return k;
+        if(digitsX >= digitsY) {
+            m = digitsX % 2 == 0 ? digitsX / 2 : digitsX / 2 + 1;
+            minDigits = digitsY;
+        } else if (digitsX < digitsY) {
+            m = digitsY % 2 == 0 ? digitsY / 2 : digitsY / 2 + 1;
+            minDigits = digitsX;
         }
 
+        //check that the diference betwen the 2 numbers is not to big
+        while(m > minDigits) {
+            m--;
+        }
+
+        //base case
+        if(digitsX == 1 || digitsY == 1) {
+            return x * y;
+        } else {
+
+            long a = (long)(Math.pow(10, ))
+        }
+
+        return 1;
     }
 
     private static long numberDigits(long n) {
@@ -91,7 +61,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
 
-        System.out.println(karatsubaMultiply(5678, 1234));
+        System.out.println(karatsubaMultiply(12345, 6789));
 
 
     }
