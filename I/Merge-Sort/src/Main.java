13ce5fb17c32152cb82b1265791ba976b16de78d
paran01c <37111262+paran01c@users.noreply.github.com>
2020-07-10 23:33:11 +0300

    works

--- I/Merge-Sort/src/Main.java
+++ I/Merge-Sort/src/Main.java
@@ -1,5 +1,77 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("work in progress");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] x = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+        }
+
+        int[] result = mergeSort(x);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] mergeSort(int[] array) {
+
+        int n = array.length;
+
+        if(n == 1) {
+            return array;
+        } else {
+            int childLength = n / 2;
+
+            //creating and populating the new arrays
+            int[] A = new int[childLength];
+            for (int i = 0; i < childLength; i++) {
+                A[i] = array[i];
+            }
+
+            int[] B = new int[n - childLength];
+            for (int i = childLength, j = 0; i < n; i++, j++) {
+                B[j] = array[i];
+            }
+
+            A = mergeSort(A);
+            B = mergeSort(B);
+
+            return merge(A, B);
+        }
+    }
+
+    public static int[] merge (int[] A, int[] B) {
+        int[] result = new int[A.length + B.length];
+        int a = 0;
+        int b = 0;
+        boolean firstEnd = false;
+        boolean secondEnd = false;
+
+        for(int i = 0; i < result.length; i++) {
+
+            if(secondEnd || (A[a] <= B[b] && !firstEnd)) {
+                result[i] = A[a];
+                a++;
+
+                if(a == A.length) {
+                    a--;
+                    firstEnd = true;
+                }
+            } else if(firstEnd || (A[a] > B[b] && !secondEnd)){
+                result[i] = B[b];
+                b++;
+
+                if(b == B.length) {
+                    b--;
+                    secondEnd = true;
+                }
+            }
+        }
+
+        return result;
+
     }
 }
